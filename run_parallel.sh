eb8fa8ebbe1987b22c440f1d36a1aef3fac5273d
danukaravishan <danukaravishan1@gmail.com>
2025-04-02 14:59:08 +1300

    Include the runs in single neurone output

--- run_parallel.sh
+++ run_parallel.sh
@@ -21,9 +21,9 @@
 
 #python src/main.py
 
-#parallel -j 10 python src/main.py --learning_rate={1} --epoch_count=50 --batch_size=32 --conv1_size=8 --conv2_size=2 --fc1_size=22 --kernal_size1={2} --kernal_size2={3} ::: 0.002 0.00286207 ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 ::: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+parallel -j 10 python src/main.py --learning_rate=0.01 --epoch_count=100 --batch_size=32 --conv1_size={1} --conv2_size={2} --fc1_size={3} --kernal_size1=4 --kernal_size2=4 ::: 8 12 16 20 24 30  :::  2 4 12 16 ::: 8 10 12 16 20 24 30
 
-python src/main.py --learning_rate=0.00286207 --epoch_count=48 --batch_size=32 --conv1_size=24 --conv2_size=4 --fc1_size=20
+#python src/main.py --learning_rate=0.00286207 --epoch_count=48 --batch_size=32 --conv1_size=24 --conv2_size=4 --fc1_size=20
 
 
 #parallel -j 10 python src/main.py --learning_rate={1} --epoch_count=100 --batch_size={2}  ::: 0.001 0.002 0.003 0.004 0.005 0.006 0.007 0.008 0.009 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1  ::: 32 64 128 256
